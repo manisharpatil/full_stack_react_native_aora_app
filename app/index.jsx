7b75111037a37302f42e3f0b2057e36a31c505a6
Manisha Patil <manisharpatil6@gmail.com>
2024-06-14 23:53:57 +0530

    feat: started implementation

--- app/index.jsx
+++ app/index.jsx
@@ -5,10 +5,14 @@ import { Link } from 'expo-router';
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-3xl font-pblack">Aora!</Text>
-      <StatusBar style="auto" />
-      <Link href="/home" style={{ color: 'blue' }}>Go to Home</Link>
-    </View>
+    <SafeAreaView className="bg-primary h-full">
+      <ScrollView contentContainerStyle={{ height: '100%'}}>
+      <View className="w-full justify-center items-center
+      h-full px-4">
+        <Image 
+        source={}/>
+      </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
