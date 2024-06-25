0f584de545acd0ae622fc66b9622ec831172206c
Manisha Patil <manisharpatil6@gmail.com>
2024-06-26 00:21:20 +0530

    feat: completed signin feature

--- app/(auth)/sign-in.jsx
+++ app/(auth)/sign-in.jsx
@@ -1,14 +1,20 @@
-import { ScrollView, Image, View, Text  } from "react-native";
+import { ScrollView, Image, View, Text } from "react-native";
 import React, { useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { Link } from 'expo-router';
 import { images } from "../../constants";
+
 import FormField from "../../components/FormField";
+import CustomButton from "../../components/CustomButton";
 
 const SignIn = () => {
   const [form, setForm] = useState({
     email: "",
     password: "",
   });
+  const [isSubmitting, setisSubmitting] = useState(false);
+
+  const submit = () => {};
 
   return (
     <SafeAreaView className="bg-primary h-full">
@@ -36,10 +42,24 @@ const SignIn = () => {
           <FormField
             title="Password"
             value={form.password}
-            handleChangeText={(e) => setForm({ ...form,
-               password: e })}
+            handleChangeText={(e) => setForm({ ...form, password: e })}
             otherStyles="mt-7"
           />
+
+          <CustomButton
+            title="Sign In"
+            handlePress={submit}
+            containerStyles="mt-7"
+            isLoading={isSubmitting}
+          />
+
+          <View className="justify-center pt-5 flex-row gap-2">
+            <Text className="text-lg text-gray-100 font-pregular">
+              Don't have account?
+            </Text>
+            <Link href="/sign-up" className="text-lg font-psemibold
+             text-secondary">Sign Up</Link>
+          </View>
         </View>
       </ScrollView>
     </SafeAreaView>
